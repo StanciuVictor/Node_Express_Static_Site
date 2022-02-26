4a598b90ea39fe9a0eee5b99b4f50e17c85b74b9
StanciuVictor <victor.stanciu@yahoo.com>
2022-02-26 23:02:44 +0200

    Add error templates and complete error handlers

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/static', express.static('public'));
 // Set routes
 app.get('/', (req, res) => {
   console.log('\x1b[33m%s\x1b[0m', '********  GET on /  ********');
-  res.locals = projects;
+  res.locals.projects = projects;
   res.render('index');
 });
 
@@ -26,15 +26,14 @@ app.get('/about', (req, res) => {
 app.get('/project/:id', (req, res) => {
   const {id} = req.params;
   console.log('\x1b[33m%s\x1b[0m', '********  GET on /project/:id  ********');
-  // console.log(projects[id]);
-  res.locals.info = projects[id];
+  res.locals.project = projects[id];
   res.render('project');
 });
 
 // Handle errors
 // 404
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error('Not found');
   err.status = 404;
   console.log('\x1b[31m%s\x1b[0m', `${err.status} ${err.message}`);
   next(err);
@@ -43,10 +42,15 @@ app.use((req, res, next) => {
 // Global error
 app.use((err, req, res, next) => {
   err.status = err.status || 500;
-  err.message = err.message || 'Oops! It looks like something went wrong on the server.';
+  err.message = err.message || 'Oops! Looks like something went wrong';
   console.log('\x1b[31m%s\x1b[0m', `${err.status} ${err.message}`);
   res.status(err.status);
-  res.render('error', {err});
+
+  if (err.status === 404) {
+    res.render('page-not-found', {err});
+  } else {
+    res.render('error', {err});
+  }
 });
 
 // Listen on port 3000
