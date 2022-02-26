e9f5d4e04e098c3dee0032c84dd5a7996e6b0949
StanciuVictor <victor.stanciu@yahoo.com>
2022-02-26 19:08:28 +0200

    Handle errors - not finished

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/static', express.static('public'));
 // Set routes
 app.get('/', (req, res) => {
   console.log('\x1b[33m%s\x1b[0m', '********  GET on /  ********');
-  res.locals = data.projects;
+  res.locals = projects;
   res.render('index');
 });
 
@@ -29,6 +29,24 @@ app.get('/project/:id', (req, res) => {
   // console.log(projects[id]);
   res.locals.info = projects[id];
   res.render('project');
+});
+
+// Handle errors
+// 404
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  console.log('\x1b[31m%s\x1b[0m', `${err.status} ${err.message}`);
+  next(err);
+});
+
+// Global error
+app.use((err, req, res, next) => {
+  err.status = err.status || 500;
+  err.message = err.message || 'Oops! It looks like something went wrong on the server.';
+  console.log('\x1b[31m%s\x1b[0m', `${err.status} ${err.message}`);
+  res.status(err.status);
+  res.render('error', {err});
 });
 
 // Listen on port 3000
